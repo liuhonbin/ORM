d6cf1ffd6814b964ea7007bfb18b726092994b98
liuhonbin <liuhonbin@LAPTOP-45AOT93R>
2017-08-16 15:59:49 +0800

    删除，修改sql语句参数处理

--- MySqlOrm/src/main/java/com/base/impl/BaseImpl.java
+++ MySqlOrm/src/main/java/com/base/impl/BaseImpl.java
@@ -14,19 +14,16 @@ import com.util.SqlUtil;
 
 public class BaseImpl implements BaseDao {
 
-	
-	
-	
 	public <T extends BaseEntity> List<T> getList(String sql, Class<T> clazz, BaseEntity baseEntity) {
 		List<T> list = new ArrayList<T>();
 		String newSql = null;
 		ResultSet rs = null;
 		try {
-		newSql = SqlUtil.sqlPrament(sql, baseEntity);
-		rs = DBUtil.query(newSql);
-		while(rs.next()){
-			list.add( DTOUtils.ReqBuildEntity(rs, clazz));
-		}
+			newSql = SqlUtil.sqlPrament(sql, baseEntity);
+			rs = DBUtil.query(newSql);
+			while (rs.next()) {
+				list.add(DTOUtils.ReqBuildEntity(rs, clazz));
+			}
 		} catch (NoSuchMethodException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -45,20 +42,19 @@ public class BaseImpl implements BaseDao {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 		return list;
 	}
 
-	
 	public <T extends BaseEntity> T getOne(String sql, Class<T> clazz, BaseEntity baseEntity) {
 		String newSql = null;
 		ResultSet rs = null;
 		try {
-		newSql = SqlUtil.sqlPrament(sql, baseEntity);
-		rs = DBUtil.query(newSql);
-		while(rs.next()){
-	     return DTOUtils.ReqBuildEntity(rs, clazz);
-		}
+			newSql = SqlUtil.sqlPrament(sql, baseEntity);
+			rs = DBUtil.query(newSql);
+			while (rs.next()) {
+				return DTOUtils.ReqBuildEntity(rs, clazz);
+			}
 		} catch (NoSuchMethodException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -77,21 +73,58 @@ public class BaseImpl implements BaseDao {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 		return null;
 	}
 
-
 	public int update(String sql, BaseEntity baseEntity) {
 		// TODO Auto-generated method stub
-		return 0;
+		int a = 0;
+		try {
+			String new_sql = SqlUtil.sqlPrament(sql, baseEntity);
+			a = DBUtil.other(new_sql);
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return a;
 	}
-
 
 	public int delete(String sql, BaseEntity baseEntity) {
 		// TODO Auto-generated method stub
-		return 0;
+		int a = 0;
+		try {
+			String new_sql = SqlUtil.sqlPrament(sql, baseEntity);
+			a = DBUtil.other(new_sql);
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return a;
 	}
 
-	
-  }
+}
